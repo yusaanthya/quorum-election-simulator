268edd3dab1042af9cd7d2b251939f1f270c86dc
Anthya1104 <anthya1104@gmail.com>
2025-06-17 16:45:31 +0800

    feat:tweak app start msg as degub level

--- internal/cobra/cobra.go
+++ internal/cobra/cobra.go
@@ -10,7 +10,7 @@ var rootCmd = &cobra.Command{
 	Use:   "app",
 	Short: "A base CLI app with Cobra and logrus",
 	Run: func(cmd *cobra.Command, args []string) {
-		logrus.Info("Hello from the base CLI app!")
+		logrus.Debugf("Hello from the base CLI app!")
 	},
 }
 
