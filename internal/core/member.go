296f9f760590917c8fe9d3edf4bbf41b916dd81b
Anthya1104 <anthya1104@gmail.com>
2025-06-23 05:50:25 +0800

    refactor:moved the redundat leader force kick member mechanism and tweak the quorum ended condition during dead member suspecting

--- internal/core/member.go
+++ internal/core/member.go
@@ -160,43 +160,19 @@ func (m *Member) monitorHeartbeats(q *Quorum) {
 					logrus.Debugf("Member %d: Added own implicit vote for suspect %d. Current votes: %v", m.ID, id, strategy.votes[id])
 					strategy.voteMutex.Unlock()
 
-					// *** EDGE CASE 2-MEMBER QUORUM LEADER FAILURE ***
+					// *** EDGE CASE 2-MEMBER QUORUM FAILURE ***
 					q.mu.Lock()
-					isTargetLeader := q.LeaderID == id
 					currentQuorumSize := len(q.members) // Get the current number of members in the quorum
+					removedMembers := q.removed
 					q.mu.Unlock()
 
-					// If the suspected member is the current leader AND the quorum size is 2,
-					// it means the leader has failed and the remaining single member cannot form a majority (2 votes needed).
-					// In this specific edge case, the quorum is unrecoverable via majority vote.
-					if isTargetLeader && currentQuorumSize == 2 {
+					// TODO: replaced the kick member mechanism as Quorum.ProposeMemberRemoval (should be the only entry points to remove member and trigger NotifyQuorumEnded())
+					if !removedMembers[id] && currentQuorumSize == 2 {
 						logrus.Warnf("Quorum unrecoverable: Leader %d failed and remaining 1-member cannot form majority. Ending quorum.", id)
 						q.cancel()
 						q.notifier.NotifyQuorumEnded()
 
 						continue
-					}
-				}
-
-				// if the suspecting dispatcher is leader and the vote could not be trigger,  force kill member from quorum
-				if q.LeaderID == m.ID {
-					strategy, ok := m.Election.(*MajorityVoteStrategy)
-					if ok {
-						strategy.voteMutex.Lock()
-						ts, exists := strategy.voteTimestamps[id]
-						if exists && m.timer.Now().Sub(ts) > VoteDecisionTimeout {
-
-							logrus.Warnf("Leader %d: Vote decision for suspect %d timed out. Forcibly removing.", m.ID, id)
-							delete(strategy.votes, id)
-							delete(strategy.voteTimestamps, id)
-							q.ProposeMemberRemoval(id)
-						}
-
-						if !exists {
-							strategy.voteTimestamps[id] = m.timer.Now()
-							logrus.Debugf("Leader %d: Started vote decision timer for suspect %d at %v", m.ID, id, strategy.voteTimestamps[id])
-						}
-						strategy.voteMutex.Unlock()
 					}
 				}
 
